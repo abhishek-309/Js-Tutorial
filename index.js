8de51a61ac2fd769bab1ebbdc1beb9b32f5576f2
Abhishek Bhatt <abhishekbhatti625@gmail.com>
2024-06-11 15:16:49 +0545

    js tutorial

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log(`Hello`);
+console.log(`I like pizza!`);
+window.alert(`This is alert!`);
+window.alert(`I like Pizza!`);
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `I like Pizza!`;
